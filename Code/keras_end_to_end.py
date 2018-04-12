23a412099cb15c00dfb5e01d53d8d36916a80121
Genius1237 <anirudhsriniv@gmail.com>
2018-04-12 21:53:11 +0530

    Implementation of End to End Encoder-Decoder system with One-Hot Encoding Results are in 3-4-18

--- Code/keras_end_to_end.py
+++ Code/keras_end_to_end.py
@@ -1,84 +1,110 @@
-'''
-- We start with input sequences from a domain (e.g. English sentences)
-    and correspding target sequences from another domain
-    (e.g. French sentences).
-- An encoder LSTM turns input sequences to 2 state vectors
-    (we keep the last LSTM state and discard the outputs).
-- A decoder LSTM is trained to turn the target sequences into
-    the same sequence but offset by one timestep in the future,
-    a training process called "teacher forcing" in this context.
-    Is uses as initial state the state vectors from the encoder.
-    Effectively, the decoder learns to generate `targets[t+1...]`
-    given `targets[...t]`, conditioned on the input sequence.
-- In inference mode, when we want to decode unknown input sequences, we:
-    - Encode the input sequence into state vectors
-    - Start with a target sequence of size 1
-        (just the start-of-sequence character)
-    - Feed the state vectors and 1-char target sequence
-        to the decoder to produce predictions for the next character
-    - Sample the next character using these predictions
-        (we simply use argmax).
-    - Append the sampled character to the target sequence
-    - Repeat until we generate the end-of-sequence character or we
-        hit the character limit.
-'''
-
 from keras.models import Model
 from keras.layers import Input, LSTM, Dense
 from random import shuffle
+from nltk.translate.bleu_score import sentence_bleu, corpus_bleu
 import numpy as np
 import pickle
+import sys
+import time
 
-batch_size = 64  # Batch size for training.
-epochs = 100  # Number of epochs to train for.
-latent_dim = 256  # Latent dimensionality of the encoding space.
-num_samples=0
-data_path=''
+batch_size = 64
+if len(sys.argv) == 2:
+    epochs = int(sys.argv[1])
+else:
+    epochs=10
+
+latent_dim = 64
+num_samples = 0
+num_test_samples=0
+data_path = ''
 
 input_texts = []
 target_texts = []
 input_words = {}
 target_words = {}
 
+test_input_texts=[]
+test_target_texts=[]
+
 def process_fra_eng():
+    global data_path
     data_path = 'fra.txt'
-    num_samples = 50000
+    global num_samples
+    num_samples = 10000
+    global num_test_samples
+    num_test_samples = num_samples // 10
+
     with open('../Data/{}'.format(data_path), 'r', encoding='utf-8') as f:
         lines = f.read().split('\n')
-    
     shuffle(lines)
-    
-    for line in lines[:min(num_samples, len(lines) - 1)]:
+
+    for line in lines[:num_samples]:
         input_text, target_text = line.split('\t')
 
         # We use "tab" as the "start sequence" character
         # for the targets, and "\n" as "end sequence" character.
+
+        target_text = '\t' + ' ' + target_text + ' '+'\n'
+
+        input_texts.append(input_text.split(' '))
+        target_texts.append(target_text.split(' '))
+
+        for word in input_text.split(' '):
+            if word not in input_words:
+                input_words[word] = len(input_words)
+
+        for word in target_text.split(' '):
+            if word not in target_words:
+                target_words[word] = len(target_words)
+
+    for line in lines[num_samples:num_samples+num_test_samples]:
+        input_text, target_text = line.split('\t')
+        target_text = '\t' + ' ' + target_text + ' '+'\n'
+
+        test_input_texts.append(input_text.split(' '))
+        test_target_texts.append(target_text.split(' '))
+
+
+def process_bible():
+    global data_path
+    data_path = 'bible_translation.pickle'
+    with open('../Data/Bible/{}'.format(data_path), 'rb') as f:
+        p = pickle.load(f)
+    
+    global num_samples
+    num_samples = 4000
+    global num_test_samples
+    num_test_samples = len(p) - 5000
+
+    for i in range(num_samples):
+        input_text,target_text=p[i]
         
         target_text = '\t' + ' ' + target_text + ' '+'\n'
         
         input_texts.append(input_text.split(' '))
         target_texts.append(target_text.split(' '))
 
-
         for word in input_text.split(' '):
             if word not in input_words:
-                input_words[word]=len(input_words)
-        
+                input_words[word] = len(input_words)
+
         for word in target_text.split(' '):
             if word not in target_words:
                 target_words[word] = len(target_words)
 
-def process_bible():
-    data_path = 'bible_translation.pickle'
-    with open('../Data/Bible/{}'.format(data_path),'rb') as f:
-        p=pickle.load(f)
-    
+    for i in range(num_samples,num_samples+num_test_samples):
+        input_text, target_text = p[i]
+        target_text = '\t' + ' ' + target_text + ' '+'\n'
+
+        test_input_texts.append(input_text.split(' '))
+        test_target_texts.append(target_text.split(' '))
+
 process_fra_eng()
 
 num_encoder_tokens = len(input_words)
 num_decoder_tokens = len(target_words)
-assert len(input_texts)==len(target_texts)
-num_examples=len(input_texts)
+assert len(input_texts) == len(target_texts)
+num_examples = len(input_texts)
 
 max_encoder_seq_length = max([len(txt) for txt in input_texts])
 max_decoder_seq_length = max([len(txt) for txt in target_texts])
@@ -90,16 +116,19 @@ print('Max sequence length for inputs:', max_encoder_seq_length)
 print('Max sequence length for outputs:', max_decoder_seq_length)
 
 
-encoder_input_data = np.zeros((num_examples, max_encoder_seq_length, num_encoder_tokens),dtype='float32')
-decoder_input_data = np.zeros((num_examples, max_decoder_seq_length, num_decoder_tokens),dtype='float32')
-decoder_target_data = np.zeros((num_examples, max_decoder_seq_length, num_decoder_tokens),dtype='float32')
+encoder_input_data = np.zeros(
+    (num_examples, max_encoder_seq_length, num_encoder_tokens), dtype='float32')
+decoder_input_data = np.zeros(
+    (num_examples, max_decoder_seq_length, num_decoder_tokens), dtype='float32')
+decoder_target_data = np.zeros(
+    (num_examples, max_decoder_seq_length, num_decoder_tokens), dtype='float32')
 
 for i in range(num_examples):
     for j in range(len(input_texts[i])):
-        encoder_input_data[i,j,input_words[input_texts[i][j]]]=1
+        encoder_input_data[i, j, input_words[input_texts[i][j]]] = 1
     for j in range(len(target_texts[i])):
-        decoder_input_data[i,j,target_words[target_texts[i][j]]]=1
-        if j>0:
+        decoder_input_data[i, j, target_words[target_texts[i][j]]] = 1
+        if j > 0:
             decoder_target_data[i, j-1, target_words[target_texts[i][j]]] = 1
 
 
@@ -116,7 +145,8 @@ decoder_inputs = Input(shape=(None, num_decoder_tokens))
 # and to return internal states as well. We don't use the
 # return states in the training model, but we will use them in inference.
 decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
-decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+decoder_outputs, _, _ = decoder_lstm(
+    decoder_inputs, initial_state=encoder_states)
 decoder_dense = Dense(num_decoder_tokens, activation='softmax')
 decoder_outputs = decoder_dense(decoder_outputs)
 
@@ -132,9 +162,14 @@ model.fit(
     batch_size=batch_size,
     epochs=epochs,
     validation_split=0.2)
-# Save model
-model.save('keras_seq2seq_{}_{}_{}_{}_{}.h5'.format(data_path,batch_size,epochs,latent_dim,num_samples))
 
+# Save model
+model.save('keras_seq2seq_{}_{}_{}_{}_{}.h5'.format(
+    data_path, batch_size, epochs, latent_dim, num_samples))
+
+with open('keras_seq2seq_{}_{}_{}_{}_{}.pickle'.format(
+        data_path, batch_size, epochs, latent_dim, num_samples), 'wb') as f:
+    pickle.dump([input_words,target_words,max_encoder_seq_length,max_decoder_seq_length],f)
 
 # Next: inference mode (sampling).
 # Here's the drill:
@@ -150,7 +185,8 @@ encoder_model = Model(encoder_inputs, encoder_states)
 decoder_state_input_h = Input(shape=(latent_dim, ))
 decoder_state_input_c = Input(shape=(latent_dim, ))
 decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
-decoder_outputs, state_h, state_c = decoder_lstm(decoder_inputs, initial_state=decoder_states_inputs)
+decoder_outputs, state_h, state_c = decoder_lstm(
+    decoder_inputs, initial_state=decoder_states_inputs)
 decoder_states = [state_h, state_c]
 decoder_outputs = decoder_dense(decoder_outputs)
 decoder_model = Model([decoder_inputs] + decoder_states_inputs,
@@ -177,7 +213,8 @@ def decode_sequence(input_seq):
     decoded_sentence = []
 
     while not stop_condition:
-        output_tokens, h, c = decoder_model.predict([target_seq] + states_value)
+        output_tokens, h, c = decoder_model.predict(
+            [target_seq] + states_value)
 
         # Sample a token
         sampled_token_index = np.argmax(output_tokens[0, -1, :])
@@ -198,12 +235,33 @@ def decode_sequence(input_seq):
 
     return decoded_sentence
 
+max_test_encoder_sequence_length = max([len(txt) for txt in test_input_texts])
 
-for seq_index in range(100):
-    # Take one sequence (part of the training set)
-    # for trying out decoding.
-    input_seq = encoder_input_data[seq_index:seq_index + 1]
-    decoded_sentence = decode_sequence(input_seq)
-    print('-')
-    print('Input sentence:', input_texts[seq_index])
-    print('Decoded sentence:', decoded_sentence)
+test_encoder_input_data = np.zeros(
+    (num_test_samples, max_test_encoder_sequence_length, num_encoder_tokens), dtype='float32')
+
+for i in range(num_test_samples):
+    for j in range(len(test_input_texts[i])):
+        if test_input_texts[i][j] in input_words:
+            test_encoder_input_data[i, j, input_words[test_input_texts[i][j]]] = 1
+
+references=[]
+hypotheses=[]
+with open('{}.txt'.format(int(time.time())),'w') as fi:
+	for i in range(num_samples):
+		decoded_sentence=decode_sequence(encoder_input_data[i:i+1])
+		
+		references.append([target_texts[i][1:-1]])
+		hypotheses.append(decoded_sentence[:-1])
+
+	print(corpus_bleu(references,hypotheses),file=fi)
+	
+	references=[]
+	hypotheses=[]	
+	for i in range(num_test_samples):
+		decoded_sentence=decode_sequence(test_encoder_input_data[i:i+1])
+		
+		references.append([test_target_texts[i][1:-1]])
+		hypotheses.append(decoded_sentence[:-1])
+
+	print(corpus_bleu(references,hypotheses),file=fi)
